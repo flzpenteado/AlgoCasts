08de76bd93316980350cde71f64f3ec5937d65c2
Fabio Penteado <flzpenteado@gmail.com>
2020-09-14 17:37:26 -0300

    [reversestring] resolved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// Solution 1
+// const reverse = str => str.split('').reverse().join('')
+
+// Solution 2
+// const reverse = str => {
+//   let reversed = ''
+//   for (i = str.length - 1; i >=0; i--) {
+//     reversed += str[i]
+//   }
+
+//   return reversed
+// }
+
+// Solution 3
+// const reverse = str => {
+//   let reversed = ''
+
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+//   return reversed
+// }
+
+const reverse = str => str.split('').reduce((acc, cur) => cur + acc, '')
 
 module.exports = reverse;
