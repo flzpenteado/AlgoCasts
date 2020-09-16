fa7bc34d63f569484b139b3a4428c6adeefee8ec
Fabio Penteado <flzpenteado@gmail.com>
2020-09-16 07:13:30 -0300

    [maxchar] resolved

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const maxChar = str => {
+  let max = 0;
+  let maxChar = ''
+  const charMap = {}
+  for (let char of str) {
+    charMap[char] =  charMap[char] ? charMap[char] + 1 : 1
+  }
 
+  for (const char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
+
+
+
+}
 module.exports = maxChar;
